079a378563f0aab688e9bd1341d09470f3b41492
yoonlight <yoonlight12@gmail.com>
2022-01-28 00:26:59 +0900

    apply pwa

--- src/index.js
+++ src/index.js
@@ -2,4 +2,14 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { mediapipe } from "./mediapipe.js"
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+            console.log('SW registered: ', registration);
+        }).catch(registrationError => {
+            console.log('SW registration failed: ', registrationError);
+        });
+    });
+}
+
 mediapipe()
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest')
 
 module.exports = {
     mode: 'none', // 아래 3개는 기본 설정
@@ -26,7 +28,34 @@ module.exports = {
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({ template: "./src/index.html" }),
+        new HtmlWebpackPlugin({ template: "./src/index.html", title: "KWIX" }),
+        new WorkboxPlugin.GenerateSW({
+            // these options encourage the ServiceWorkers to get in there fast
+            // and not allow any straggling "old" SWs to hang around
+            clientsClaim: true,
+            skipWaiting: true,
+        }),
+        new WebpackPwaManifest({
+            name: 'KWIX',
+            short_name: 'KWIX',
+            description: 'KWIX Progressive Web App!',
+            background_color: '#ffffff',
+            crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+            icons: [
+                {
+                    src: path.resolve('src/assets/icon-1.png'),
+                    sizes: [512], // multiple sizes
+                    ios: true
+                },
+                {
+                    src: path.resolve('src/assets/icon-2.png'),
+                    size: '192x192', // you can also use the specifications pattern
+                    ios: true
+                },
+            ],
+            ios: true,
+            publicPath: '.'
+        })
     ],
     optimization: {
         splitChunks: { chunks: 'all' },
